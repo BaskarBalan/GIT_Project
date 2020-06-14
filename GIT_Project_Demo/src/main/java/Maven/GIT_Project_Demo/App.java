8dc1878453a055aaeb60a1b5d7e436d2ce32d9af
Baskar Balan <Baskar Balan@DESKTOP-LUPQBPU>
2020-06-14 11:40:07 +0100

    Nintia

---
+++ GIT_Project_Demo/src/main/java/Maven/GIT_Project_Demo/App.java
@@ -0,0 +1,16 @@
+package Maven.GIT_Project_Demo;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public void main( )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
